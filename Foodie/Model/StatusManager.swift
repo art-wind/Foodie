9c145efbf007720131ea3aabfbf3f4e9095fba92
huzhenxuan17 <huzhenxuan171@gmail.com>
2015-05-23 23:23:26 +0800

    Modify status&&user Manager
    2 function added in each Manager

--- Foodie/Model/StatusManager.swift
+++ Foodie/Model/StatusManager.swift
@@ -7,3 +7,62 @@
 //
 
 import Foundation
+
+class StatusManager{
+    
+    //发状态  TODO
+    func postStateRequest()->NSMutableURLRequest{
+        let urlStr = "http://115.29.138.163:8080/Foodie/StatusService"
+        let url = NSURL(string:urlStr)
+        let urlRequest = NSMutableURLRequest(URL: url!)
+        urlRequest.HTTPMethod = "Post"
+        return urlRequest
+        
+    }
+    
+    //为状态点赞
+    func admireStatusRequest(status_id:Int)->NSMutableURLRequest{
+        let urlStr = "http://115.29.138.163:8080/Foodie/AdmireStatusService"
+        let url = NSURL(string:urlStr)
+        let urlRequest = NSMutableURLRequest(URL: url!)
+        urlRequest.HTTPMethod = "Post"
+        urlRequest.setValue(status_id, forKey: "status_id")
+        return urlRequest
+        
+    }
+
+    //朋友圈
+    func momentsStatusRequest(user_id:Int, pageNum:Int)->NSMutableURLRequest{
+        let urlStr = "http://115.29.138.163:8080/Foodie/MomentsService"
+        let url = NSURL(string:urlStr)
+        let urlRequest = NSMutableURLRequest(URL: url!)
+        urlRequest.HTTPMethod = "Post"
+        urlRequest.setValue(user_id, forKey: "user_id")
+        urlRequest.setValue(pageNum, forKey: "pageNum")
+        return urlRequest
+    }
+    
+    //根据id找到状态
+    func statusByUserRequset(user_id:Int, pageNum:Int)->NSMutableURLRequest{
+        let urlStr = "http://115.29.138.163:8080/Foodie/FriendsStatusService"
+        let url = NSURL(string:urlStr)
+        let urlRequest = NSMutableURLRequest(URL: url!)
+        urlRequest.HTTPMethod = "Post"
+        urlRequest.setValue(user_id, forKey: "user_id")
+        urlRequest.setValue(pageNum, forKey: "pageNum")
+        return urlRequest
+    }
+    
+    //type2
+    func getStatusFromData(data:NSData)->Status{
+        let xml = SWXMLHash.parse(data)
+        return Status.convertStatus(xml)
+        
+    }
+    
+    func getStatusListFromData(data:NSData)->[Status]{
+        let xml = SWXMLHash.parse(data)
+        return Status.convertStatusList(xml)
+        
+    }
+}
--- Foodie/Model/User.swift
+++ Foodie/Model/User.swift
@@ -16,17 +16,37 @@ class User {
     var nickname:String?
     var phoneNum:String?
     
-    init(id:Int, username:String, password:String,icon:String,nickname:String,phoneNum:String){
-        self.id = id
-        self.username = username
-        self.password = password
-        self.icon = icon
-        self.nickname = nickname
-        self.phoneNum = phoneNum
-        
+    //    init(id:Int, username:String, password:String,icon:String,nickname:String,phoneNum:String){
+    //        self.id = id
+    //        self.username = username
+    //        self.password = password
+    //        self.icon = icon
+    //        self.nickname = nickname
+    //        self.phoneNum = phoneNum
+    //
+    //    }
+    
+    
+    
+    
+    init(xml: XMLIndexer){
+        self.id = xml["root"]["header"]["user_id"].element?.text?.toInt()
+        self.nickname = "HZX_TEST"
+    }
+    
+    class func convertUser(xml: XMLIndexer) -> User{
+        //TODO xml格式
+        var user = User(xml: xml)
+        return user
+    }
+
+    
+    class func convertUserList(xml: XMLIndexer) -> [User]{
+        //TODO xml格式
+        var userList = [User]()
+        return userList
     }
     
     
-  
     
 }
